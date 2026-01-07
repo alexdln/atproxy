0dba73fa9109a511b1e7648b59ad94a4de6d9876
Vordgi <sasha2822222@gmail.com>
2026-01-07 12:10:57 +0000

    ap-1 configure repo

---
+++ eslint.config.mjs
@@ -0,0 +1,21 @@
+import { defineConfig, globalIgnores } from "eslint/config";
+import prettier from "eslint-config-prettier";
+import tseslint from "typescript-eslint";
+import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
+
+const eslintConfig = defineConfig([
+    ...tseslint.configs.recommended,
+    eslintPluginPrettierRecommended,
+    prettier,
+    {
+        files: ["*.json"],
+        plugins: eslintPluginPrettierRecommended.plugins,
+        rules: {
+            "prettier/prettier": ["error"],
+            "@typescript-eslint/no-unused-expressions": 0,
+        },
+    },
+    globalIgnores(["**/dist/**", "**/node_modules/**"]),
+]);
+
+export default eslintConfig;
